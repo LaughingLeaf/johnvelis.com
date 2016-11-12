9e26f38efb49f9a38fb5963d55a04bf59bba5d90
John Velis <john@johnvelis.com>
2016-11-12 08:54:53 -0500

    added code.org links

---
+++ johnvelis.com/Controllers/GirlTech16FallController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace johnvelis.com.Controllers
+{
+    public class GirlTech16FallController : Controller
+    {
+        // GET: GirlTech16Fall
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChangingTheCurve()
+        {
+            return View();
+        }
+    }
+}
---
+++ johnvelis.com/Controllers/TechDay16Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace johnvelis.com.Controllers
+{
+    public class TechDay16Controller : Controller
+    {
+        // GET: TechDay16
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
